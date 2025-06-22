d9223bad886c37f007da1f2d4aa626f39f90eea7
Liam Tingley <liamtingley@gmail.com>
2025-06-22 17:55:19 -0300

    Implemented Exhibition Race Filtration
    Successfully implemented initial filtration handling omission or inclusion of exhibition races (Clash, Duels, All-Star, etc.).
    
    Also added additional attributes to Race class.

--- RaceData.py
+++ RaceData.py
@@ -7,7 +7,7 @@ import json, datetime, time
 
 class Race:
 
-    def __init__(self, raceSeason: int, seriesID: int, round: int):
+    def __init__(self, raceSeason: int, seriesID: int, round: int, includeExhibitions: bool = False):
         
         self._raceSeason: int = raceSeason
         self._seriesID: int = seriesID
@@ -16,55 +16,85 @@ class Race:
         # Fetching annual data from race_list_basic.json.
         url: str = f"https://cf.nascar.com/cacher/{raceSeason}/race_list_basic.json"
         response: json = urlopen(url)
-        data: dict = json.loads(response.read())
-        
+        races: list = json.loads(response.read())
+        seriesRaces: list = races[f"series_{seriesID}"]
+        raceIndex: int = 0
+
+
+        # If exhibitions are not included, find the nth occurrence in for which "race_type_id" is 1.
+        if not includeExhibitions:
+            
+            pointsRaceCounter: int = 0
+
+            for race in seriesRaces:
+
+                if race["race_type_id"] == 1:
+                    pointsRaceCounter += 1
+                else:
+                    pass
+
+                if pointsRaceCounter == round:
+                    raceIndex = seriesRaces.index(race)
+                    break
+        else:
+            raceIndex = round - 1
+
+        #region Fetching and assigning initial races values.
         # Locating specific race information basic on other provided parameters.
-        self._raceID: int = data[f"series_{seriesID}"][round - 1]["race_id"]
-        self._raceName: str = data[f"series_{seriesID}"][round - 1]["race_name"]
-        self._raceTypeID: int = data[f"series_{seriesID}"][round - 1]["race_type_id"]
-        self._restrictorPlate: bool = data[f"series_{seriesID}"][round - 1]["restrictor_plate"]
-        self._trackID: int = data[f"series_{seriesID}"][round - 1]["track_id"]
-        self._trackName: str = data[f"series_{seriesID}"][round - 1]["track_name"]
-        self._dateScheduled: datetime = data[f"series_{seriesID}"][round - 1]["date_scheduled"]
-        self._raceDate: datetime = data[f"series_{seriesID}"][round - 1]["race_date"]
-        self._qualifyingDate: datetime = data[f"series_{seriesID}"][round - 1]["qualifying_date"]
-        self._tuneInDate: datetime = data[f"series_{seriesID}"][round - 1]["tunein_date"]
-        self._scheduledDistance: float = data[f"series_{seriesID}"][round - 1]["scheduled_distance"]
-        self._actualDistance: float = data[f"series_{seriesID}"][round - 1]["actual_distance"]
-        self._scheduledLaps: int = data[f"series_{seriesID}"][round - 1]["scheduled_laps"]
-        self._actualLaps: int = data[f"series_{seriesID}"][round - 1]["actual_laps"]
-        self._stage1Laps: int = data[f"series_{seriesID}"][round - 1]["stage_1_laps"]
-        self._stage2Laps: int = data[f"series_{seriesID}"][round - 1]["stage_2_laps"]
-        self._stage3Laps: int = data[f"series_{seriesID}"][round - 1]["stage_3_laps"]
-        self._carCount: int = data[f"series_{seriesID}"][round - 1]["number_of_cars_in_field"]
-        self._poleWinnerDriverID: int = data[f"series_{seriesID}"][round - 1]["pole_winner_driver_id"]
-        self._poleWinnerSpeed: float = data[f"series_{seriesID}"][round - 1]["pole_winner_speed"]
-        self._numberOfLeadChanges: int = data[f"series_{seriesID}"][round - 1]["number_of_lead_changes"]
-        self._numberOfLeaders: int = data[f"series_{seriesID}"][round - 1]["number_of_leaders"]
-        self._numberOfCautions: int = data[f"series_{seriesID}"][round - 1]["number_of_cautions"]
-        self._numberOfCautionLaps: int = data[f"series_{seriesID}"][round - 1]["number_of_caution_laps"]
-        self._averageSpeed: float = data[f"series_{seriesID}"][round - 1]["average_speed"]
-        self._totalRaceTime: time = data[f"series_{seriesID}"][round - 1]["total_race_time"]
-        self._marginOfVictory: float = data[f"series_{seriesID}"][round - 1]["margin_of_victory"]
-        self._racePurse: float = data[f"series_{seriesID}"][round - 1]["race_purse"]
-        self._raceComments: str = data[f"series_{seriesID}"][round - 1]["race_comments"]
-        self._attendance: int = data[f"series_{seriesID}"][round - 1]["attendance"]
-        self._infractions: list = data[f"series_{seriesID}"][round - 1]["infractions"]
-        self._schedule: list = data[f"series_{seriesID}"][round - 1]["schedule"]
-        self._radioBroadcaster: str = data[f"series_{seriesID}"][round - 1]["radio_broadcaster"]
-        self._tvBroadcaster: str = data[f"series_{seriesID}"][round - 1]["television_broadcaster"]
-        self._satelliteRadioBroadcaster: str = data[f"series_{seriesID}"][round - 1]["satellite_radio_broadcaster"]
-        self._masterRaceID: int = data[f"series_{seriesID}"][round - 1]["master_race_id"]
-        self._inspectionComplete: bool = data[f"series_{seriesID}"][round - 1]["inspection_complete"]
-        self._playoffRound: int = data[f"series_{seriesID}"][round - 1]["playoff_round"]
-        # self._info: Info
+        self._raceID: int = seriesRaces[raceIndex]["race_id"]
+        self._raceName: str = seriesRaces[raceIndex]["race_name"]
+        self._raceTypeID: int = seriesRaces[raceIndex]["race_type_id"]
+        self._restrictorPlate: bool = seriesRaces[raceIndex]["restrictor_plate"]
+        self._trackID: int = seriesRaces[raceIndex]["track_id"]
+        self._trackName: str = seriesRaces[raceIndex]["track_name"]
+        self._dateScheduled: datetime = seriesRaces[raceIndex]["date_scheduled"]
+        self._raceDate: datetime = seriesRaces[raceIndex]["race_date"]
+        self._qualifyingDate: datetime = seriesRaces[raceIndex]["qualifying_date"]
+        self._tuneInDate: datetime = seriesRaces[raceIndex]["tunein_date"]
+        self._scheduledDistance: float = seriesRaces[raceIndex]["scheduled_distance"]
+        self._actualDistance: float = seriesRaces[raceIndex]["actual_distance"]
+        self._scheduledLaps: int = seriesRaces[raceIndex]["scheduled_laps"]
+        self._actualLaps: int = seriesRaces[raceIndex]["actual_laps"]
+        self._stage1Laps: int = seriesRaces[raceIndex]["stage_1_laps"]
+        self._stage2Laps: int = seriesRaces[raceIndex]["stage_2_laps"]
+        self._stage3Laps: int = seriesRaces[raceIndex]["stage_3_laps"]
+        self._carCount: int = seriesRaces[raceIndex]["number_of_cars_in_field"]
+        self._poleWinnerDriverID: int = seriesRaces[raceIndex]["pole_winner_driver_id"]
+        self._poleWinnerSpeed: float = seriesRaces[raceIndex]["pole_winner_speed"]
+        self._numberOfLeadChanges: int = seriesRaces[raceIndex]["number_of_lead_changes"]
+        self._numberOfLeaders: int = seriesRaces[raceIndex]["number_of_leaders"]
+        self._numberOfCautions: int = seriesRaces[raceIndex]["number_of_cautions"]
+        self._numberOfCautionLaps: int = seriesRaces[raceIndex]["number_of_caution_laps"]
+        self._averageSpeed: float = seriesRaces[raceIndex]["average_speed"]
+        self._totalRaceTime: time = seriesRaces[raceIndex]["total_race_time"]
+        self._marginOfVictory: float = seriesRaces[raceIndex]["margin_of_victory"]
+        self._racePurse: float = seriesRaces[raceIndex]["race_purse"]
+        self._raceComments: str = seriesRaces[raceIndex]["race_comments"]
+        self._attendance: int = seriesRaces[raceIndex]["attendance"]
+        self._infractions: list = seriesRaces[raceIndex]["infractions"]
+        self._schedule: list = seriesRaces[raceIndex]["schedule"]
+        self._radioBroadcaster: str = seriesRaces[raceIndex]["radio_broadcaster"]
+        self._tvBroadcaster: str = seriesRaces[raceIndex]["television_broadcaster"]
+        self._satelliteRadioBroadcaster: str = seriesRaces[raceIndex]["satellite_radio_broadcaster"]
+        self._masterRaceID: int = seriesRaces[raceIndex]["master_race_id"]
+        self._inspectionComplete: bool = seriesRaces[raceIndex]["inspection_complete"]
+        self._playoffRound: int = seriesRaces[raceIndex]["playoff_round"]
+        self._isQualifyingRace: bool = seriesRaces[raceIndex]["is_qualifying_race"]
+        self._qualifyingRaceNo: int = seriesRaces[raceIndex]["qualifying_race_no"]
+        self._qualifyingRaceID: int = seriesRaces[raceIndex]["qualifying_race_id"]
+        self._hasQualifying: bool = seriesRaces[raceIndex]["has_qualifying"]
+        self._winnerDriverID: int = seriesRaces[raceIndex]["winner_driver_id"]
+        self._poleWinnerLaptime: time = seriesRaces[raceIndex]["pole_winner_laptime"]
         # self._performance: Performance
+        #endregion
 
     ###########################################################################
     #                                                                         #
     #                              Getter Methods                             #
     #                                                                         #
     ###########################################################################
+
+    #region Getter method properties for data retrieval from race_list_basic.json.
 
     @property
     def raceID(self) -> int:
@@ -214,20 +244,42 @@ class Race:
     def playoffRound(self) -> int:
         return self._playoffRound
     
-    # The following properties are commented out because the Info and Performance classes have yet to be implemented.
-
-    # @property
-    # def info(self):
-    #     return self._info
+    @property
+    def isQualifyingRace(self) -> bool:
+        return self._isQualifyingRace
+    
+    @property
+    def qualifyingRaceNo(self) -> int:
+        return self._qualifyingRaceNo
+    
+    @property
+    def qualifyingRaceID(self) -> int:
+        return self._qualifyingRaceID
+    
+    @property
+    def hasQualifying(self) -> bool:
+        return self._hasQualifying
+    
+    @property
+    def winnerDriverID(self) -> int:
+        return self._winnerDriverID
+    
+    @property
+    def poleWinnerLaptime(self) -> time:
+        return self._poleWinnerLaptime
+    
+    # The following property is commented out because the Performance class has yet to be implemented.
     
     # @property
     # def performance(self):
     #     return self._performance
 
+    #endregion
+
 if __name__ == "__main__":
 
     # Test instantiation of Race object.
-    race = Race(2023, 1, 4)
+    race = Race(2023, 2, 1)
 
-    from inspect import getmembers
-    print(getmembers(race))
+    print(race.raceID)
+    print(race.raceName)
